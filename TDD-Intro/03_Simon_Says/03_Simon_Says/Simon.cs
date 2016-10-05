cb7914e00f183ec64c8dadf64db53c1f916c4595
moegsvin <black@DESKTOP-IAP7KVH>
2016-10-05 20:30:32 +0200

    03

--- TDD-Intro/03_Simon_Says/03_Simon_Says/Simon.cs
+++ TDD-Intro/03_Simon_Says/03_Simon_Says/Simon.cs
@@ -1,46 +1,73 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace _03_Simon_Says
 {
     internal class Simon
     {
+        private string[] smallWords = {"the", "over", "in", "and"};
+
         public Simon()
         {
         }
 
         internal string Echo(string v)
         {
-            throw new NotImplementedException();
+            return v.ToLower();
         }
 
         internal string Shout(string v)
         {
-            throw new NotImplementedException();
+            return v.ToUpper();
         }
 
         internal string Repeat(string v)
         {
-            throw new NotImplementedException();
+            return v + " " + v;
         }
 
         internal string Repeat(string v, int i)
         {
-            throw new NotImplementedException();
+            string s = v;
+
+            while(i-- > 1)
+            {
+                s += " " + v;
+            }
+
+            return s;
         }
 
         internal string StartOfWord(string v, int i)
         {
-            throw new NotImplementedException();
+            return v.Substring(0,i);
         }
 
         internal string FirstWord(string v)
         {
-            throw new NotImplementedException();
+            string[] s = v.Split();
+            return s[0];
         }
 
         internal string Titleize(string v)
         {
-            throw new NotImplementedException();
+            List<string> l = new List<string>();
+
+            foreach(string word in v.Split())
+            {
+                if(smallWords.Contains(word))
+                    l.Add(word);
+                else
+                    l.Add(Capitalize(word));
+            }
+
+            return Capitalize(String.Join(" ", l));
+        }
+
+        internal string Capitalize(string v)
+        {
+            return v.Substring(0,1).ToUpper() + v.Substring(1);
         }
     }
 }
